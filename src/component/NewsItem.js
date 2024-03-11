b3f3eba6e68c95cfe2cc5dbbac510aa1a90e7add
Aditya-Patel24 <adityasp243@gmail.com>
2024-03-11 22:29:40 +0530

    change

--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -10,7 +10,7 @@ export class NewsItem extends Component {
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{description}</p>
-    <a href={urlId} target="_blank" className="btn btn-primary btn-sm">Read More</a>
+    <a href={urlId} target="_blank" rel="noreferrer" className="btn btn-primary btn-sm">Read More</a>
   </div>
 </div>
       </div>
